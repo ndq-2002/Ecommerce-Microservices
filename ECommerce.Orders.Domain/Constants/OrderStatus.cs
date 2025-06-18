2b6c258173bd2c93c7f8c75535887ad59d713785
quang <nguyendacquang27042002@gmail.com>
2025-06-18 17:45:47 +0700

    initial Orders

---
+++ ECommerce.Orders.Domain/Constants/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Orders.Domain.Constants
+{
+    public enum OrderStatus
+    {
+        Created,
+        Confirmed,
+        Shipped,
+        Completed,
+        Cancelled
+    }
+}
---
+++ ECommerce.Orders.Domain/Models/OrderItems.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Orders.Domain.Models
+{
+    public class OrderItems
+    {
+        public string Id { get; set; }
+        public string OrderId { get; set; }
+        public string ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
