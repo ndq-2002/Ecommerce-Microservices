e3bede0b0d3d7ae7d0daf900a36103dc0c946a78
quang <nguyendacquang27042002@gmail.com>
2025-06-20 16:31:25 +0700

    feature: config orders

---
+++ ECommerce.Carts.Domain/IRepositories/ICatalogRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerce.Carts.Domain.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Carts.Domain.IRepositories
+{
+    public interface ICatalogRepository
+    {
+        Task<List<ProductCatalogViewModel>> GetDetailListPrductsAsync(string id);
+    }
+}
---
+++ ECommerce.Carts.Domain/IServices/IRedisCartService.cs
@@ -0,0 +1,16 @@
+﻿using ECommerce.Carts.Domain.ModelMetas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Carts.Domain.IServices
+{
+    public interface IRedisCartService
+    {
+        Task<List<CartItemMeta>> GetCartItemsAsync(string userId);
+        Task SetCartAsync(string userId, List<CartItemMeta> items);
+        Task ClearCartAsync(string userId);
+    }
+}
